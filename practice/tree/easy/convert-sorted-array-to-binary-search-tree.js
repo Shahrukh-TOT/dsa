9e403f777f463afc1655b500e26212902fe5764f
Shahrukh Mansuri <shahrukh.mansuri@theonetechnologies.co.in>
2021-11-10 19:26:27 +0530

    Add solved problems

---
+++ practice/tree/easy/convert-sorted-array-to-binary-search-tree.js
@@ -0,0 +1,18 @@
+//Definition for a binary tree node.
+function TreeNode(val, left, right) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+}
+
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+var sortedArrayToBST = function (nums) {
+
+    
+
+};
+
+console.log(JSON.stringify(sortedArrayToBST([-10, -3, 0, 5, 9])));
